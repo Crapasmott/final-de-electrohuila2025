481971581478137a192fb433bb1fa07537af50b1
Alex <crapas76@gmail.com>
2025-05-06 16:23:38 -0500

    Descripción de los cambios

--- src/app/comunicados/layout.jsx
+++
@@ -1,7 +0,0 @@
-export default function ComunicadosLayout({ children }) {
-    return (
-      <div className="comunicados-layout">
-        {children}
-      </div>
-    );
-  }
--- src/components/SkeletonLoader.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import React from "react";
-
-export default function SkeletonLoader() {
-  return (
-    <div className="animate-pulse bg-gray-300 rounded-lg w-full h-64"></div>
-  );
-}
